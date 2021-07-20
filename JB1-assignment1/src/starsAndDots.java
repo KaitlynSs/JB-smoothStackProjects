d76e41bd30b0cdae41fabef413aca53e281abba0
KaitlynSs <katie.spehn@smoothstack.com>
2021-07-20 01:30:39 -0500

    edits to assignment 1

--- JB1-assignment1/src/starsAndDots.java
+++ JB1-assignment1/src/starsAndDots.java
@@ -1,48 +1,11 @@
-import java.io.InputStreamReader;
-import java.util.Scanner;
-
 public class starsAndDots {
 
-	private static Scanner scanner;
-
 	public static void main(String args[]) {
-		scanner = new Scanner(new InputStreamReader(System.in));
-		
-		System.out.println("What pattern would you like to see...\n"
-				+ "1) star pyrimid \n"
-				+ "2) reverse star pyrimid \n"
-				+ "3) star pyrimid, centered \n"
-				+ "4) reverse star pyrimid, centered \n"
-				+ "5) print all paterns in listed order, 1-4");
-		int input = scanner.nextInt();
-		
-		if (input >= 1 && input <= 5) {
-			System.out.println("\nOk, pattern " + input + " selected\n");	
-			
-			switch (input) {
-			case 1:
-				starPyrimid();
-				break;
-			case 2:
-				reverseStarPyrimid();
-				break;
-			case 3:
-				starPyrimidC();
-				break;
-			case 4:
-				reverseStarPyrimidC();
-				break;
-			case 5:
+		//functions split for debugging purposes
 				starPyrimid(); //1
 				reverseStarPyrimid(); //2
 				starPyrimidC(); //3
 				reverseStarPyrimidC(); //4
-				break;
-			}
-
-		} else {
-			System.out.println("\nnot a valid input, please try again\n");
-		}
 	}
 	
 	public static void starPyrimid() {
