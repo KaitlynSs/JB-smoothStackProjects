93d3c5eb5f031774230a21d3e5021f578c2c0b66
KaitlynSs <katie.spehn@smoothstack.com>
2021-07-25 16:01:24 -0500

    Java basics 3: assignment 1 (revised)

--- JB3-assignment1/src/fileReader.java
+++ JB3-assignment1/src/fileReader.java
@@ -1,52 +1,62 @@
 import java.io.*;
 import java.util.Scanner;
 
-//not finnished
-
 public class fileReader {
 	private static Scanner scanner;
-	
+		
 	public static void main (String[] args) {
 		
 		scanner = new Scanner(new InputStreamReader(System.in));
-		boolean noDir = false;
-		int space = 1;
-		String addedSpace = "";
-		String dArray[] = {" "};
+		boolean valid = false;
+		int Level = 1;
 		
 		System.out.println("Input a directory...");
-		File fD = new File(scanner.nextLine());
 		
-		FileFilter dirFilter = new FileFilter() {
-			public boolean accept(File fD) {
-				return fD.isDirectory();
+		while (valid == false) {
+			
+			File fD = new File(scanner.nextLine());
+			if (fD.exists() && fD.isDirectory()) { 
+				
+				System.out.println("\ndirectory name accepted, now printing all files in " + fD.getName() + "...");
+			    System.out.println("+ "  + fD.getName() + "   (Main Dir)");
+			    
+			    printFiles(fD, Level);
+		        valid = true;
+			} else if (fD.isFile()) {
+				
+				System.out.println("Error: not a directory, try again");
+			} else if (!fD.exists()) {
+				
+				System.out.println("Error: enrty does not exist, try again");
 			}
-		};
-		
-		FileFilter fileFilter = new FileFilter() {
-			public boolean accept(File fD) {
-				return !fD.isDirectory();
-			}
-		};
-	
-	if (fD.isDirectory()) {
-		while (noDir == false) {
-			File[] fileArray = fD.listFiles(fileFilter);
-			File[] dirArray = fD.listFiles(dirFilter);
-
-			noDir = true;
-			
-			System.out.println("List of files and directories in the specified directory:\n");
-			System.out.println("Dir: " + fD.getName());
-			
-			for (int j = 0; j  < space; j++) {
-  			  addedSpace += "  ";
-  		  }
-		      for(int i = 0; i < fileArray.length; i++) {
-		    		  System.out.println(addedSpace + "- " + fileArray[i].getName());
-		      }
-			
 		}
-		      }
 	}
+	
+	public static void printFiles(File d, int l) {
+		
+		File[] allFiles = d.listFiles();
+		String space = getSpacing(l);
+		
+		for (int i = 0; i < allFiles.length; i ++) {
+			
+			if (allFiles[i].isDirectory()) {
+				
+				System.out.println(space + "+ " + allFiles[i].getName() + "   (Dir)");
+				l++;
+				printFiles(allFiles[i], l);
+			}else {
+				
+				System.out.println(space + "- " + allFiles[i].getName());
+			}
+		}
 	}
+
+	public static String getSpacing(int l) {
+		String s = "";
+		
+		for (int i = 0; i < l; i++) {
+			s += "   ";
+		}
+		return s;
+	}
+}
